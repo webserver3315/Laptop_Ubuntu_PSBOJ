3e5a35e069b972b4c661e2e2dc243b6cc7d2c8ac
webserver3315 <webserver3315@naver.com>
2020-04-25 20:46:04 +0900

    7W SiSoSil HW Exercise Completed

--- SKKU/SiSoSil1/7W/SiSoSil7W_1.c
+++ SKKU/SiSoSil1/7W/SiSoSil7W_1.c
@@ -35,25 +35,28 @@ int main(){
         char c='1';
         char* like="like";
         char* want="want";
-        // fscanf(ifp, " %[^ ]", buf);
-        fscanf(ifp, "%s", buf);
+
+        fscanf(ifp, "%[^ ]", buf);
+        // fscanf(ifp, "%s", buf);
         if(strcmp(buf, like)==0){
             buf[0]='l'; buf[1]='o'; buf[2]='v'; buf[3]='e';
-            printf("%s",buf);
+            // printf("%s",buf);
             fprintf(ofp, "%s",buf);
         }
         else if(strcmp(buf,want)==0){
             buf[0]='n'; buf[1]='e'; buf[2]='e'; buf[3]='d';
-            printf("%s",buf);
+            // printf("%s",buf);
             fprintf(ofp, "%s",buf);
         }
         else{
-            printf("%s",buf);
+            // printf("%s",buf);
             fprintf(ofp, "%s",buf);
         }
 
         if(!feof(ifp)){
-            
+            fscanf(ifp,"%c",&c);
+            // printf("진입, c는 %d\n",c);
+            fprintf(ofp, "%c",c);
         }
     }
     fclose(ifp);
